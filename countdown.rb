611b7b6cf45a90337eedf20d1404ec54720826c3
Brian Vehige <brianvehige@gmail.com>
2020-02-15 23:25:06 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,12 +1,6 @@
 #write your code here
 
-def countdown(integer)
-  while integer > 0
-    puts "#{integer}SECOND(S)!"
-    integer -= 1
-end
-"HAPPY NEW YEAR!"
-end
+
 
 def countdown_with_sleep(integer)
   while integer > 0 
