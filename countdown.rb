d368eef45479f3d8a4e0f3729dae63f49189bc55
Brian Vehige <brianvehige@gmail.com>
2020-02-15 23:15:40 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,8 +4,8 @@ def countdown(integer)
   x = (integer)
   while x > 0
     n = x - 1
-    print "#{x} SECOND(S)!\n#{n}"
+    print "#{x} SECOND(S)!"
     x -= 1
 end
-puts "HAPPY NEW YEAR!"
 end
+puts "HAPPY NEW YEAR!"
